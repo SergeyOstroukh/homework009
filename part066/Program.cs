e652cc88b87dfd8d893f791e2c7f89e1001f1892
Sergey <hardmass3ffect@gmail.com>
2023-02-14 22:37:27 +0300

    lasthomework

---
+++ part066/Program.cs
@@ -0,0 +1,25 @@
+﻿
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// Выполнить с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+int SumNumbers(int num1, int num2)
+{
+
+    if (num1 > num2)
+        return num1 + SumNumbers(num1 - 1, num2);
+
+    if (num1 == num2) return num2;
+
+    return num1 + SumNumbers(num1 + 1, num2);
+
+
+}
+
+Console.Write("Введите число M: ");
+int numberM = int.Parse(Console.ReadLine());
+Console.Write("Введите число N: ");
+int numberN = int.Parse(Console.ReadLine());
+int sumer = SumNumbers(numberM, numberN);
+Console.Write(sumer);
---
+++ part068/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int Akkerman(int num1, int num2)
+{
+    if(num1 == 0) return num2 +1;
+    else if(num2 ==0) return Akkerman(num1 -1, 1);
+    return Akkerman(num1-1, Akkerman(num1, num2 -1));
+}
+
+Console.Write("Введите число M: ");
+int numberM = int.Parse(Console.ReadLine());
+Console.Write("Введите число N: ");
+int numberN = int.Parse(Console.ReadLine());
+int akkerman = Akkerman(numberM, numberN);
+Console.WriteLine(akkerman);
